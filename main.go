b50c0dfe399a9efeffd9efb5067d9d672bb3cc48
Mauricio Rojas <mrojasb2000@gmail.com>
2022-02-04 23:51:26 -0300

    Added layout main

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+var responses = make([]*Rsvp, 0, 10)
+
 func main() {
 	fmt.Println("TODO: add some features")
 }
